00f6c8303ad80e3b3c162cd8076168401ecb6050
Vadim <vadlen2149@gmail.com>
2023-04-08 23:49:28 +0300

    implemented design and MarvelServices

---
+++ src/services/MarvelServices.js
@@ -0,0 +1,35 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=ba2b48306bc07df9a2133fb375959b80'
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Couldn't  fetch ${url}, status $`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        const res = this.getResource(`${this._apiBase}characters?limit=9&offset=200&${this._apiKey}`); 
+        return res.data.results.map(this._transformCharacter)
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description || 'Description for this character will be added later!',
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url 
+        }
+    }
+}
+
+export default MarvelService;
