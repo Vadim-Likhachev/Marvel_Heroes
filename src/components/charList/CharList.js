f099be68a6f0b3183a0a836d2cfd932e935b8424
Vadim <vadlen2149@gmail.com>
2023-04-10 09:32:47 +0300

    developed logic for the component CharList

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,76 @@
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import { Component } from 'react';
 
-const CharList = () => {
-    return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
+import MarvelService from '../../services/MarvelService';
+import { SpinnerCircular } from 'spinners-react';
+import Error from '../error/error';
+
+import './charList.scss';
+
+class CharList  extends Component {
+
+    state = {
+        chars: [],
+        loading: true,
+        error: false
+
+    }
+
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChars()
+    }
+
+    updateChars = () => {
+        this.marvelService
+            .getAllCharacters()
+            .then(this.onCharsLoaded)
+    }
+
+    onCharsLoaded = (chars) => {
+        this.setState({
+            chars: [...chars],
+            loading: false
+        })
+    }
+
+    render() {
+        const {chars, loading, error} = this.state;
+
+        const spinner = loading ? <div className='randomchar__spinner'><SpinnerCircular style={{margin: 'auto'}} size={200} color={'#9F0013'}/></div> : null;
+        const errorMessage = error ? <Error/> : null;
+        const content = !(spinner || errorMessage) ? <View chars={chars}/> : null;
+        
+        return (
+            <div className="char__list">
+                { spinner }
+                { errorMessage }
+                { content }
+                
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
+
 }
 
+const View = ({chars}) => {
+
+    return (
+        <ul className="char__grid">
+            {
+                chars.map((char, index) => {
+                    return (
+                        <li key={index} className="char__item">
+                            <img src={char.thumbnail} alt={char.name}/>
+                            <div className="char__name">{char.name}</div>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
 export default CharList;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,18 +1,13 @@
 import { Component } from 'react';
 
 import { SpinnerCircular } from 'spinners-react';
-import MarvelService from '../../services/MarvelServices';
+import MarvelService from '../../services/MarvelService';
 import Error from '../error/error'
 
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props)
-        this.updateChar()
-    }
-
     state = {
         char: {},
         loading: true,
@@ -20,6 +15,10 @@ class RandomChar extends Component {
     }
 
     marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar()
+    }
 
     onCharLoaded = (char) => {
         this.setState({
@@ -63,7 +62,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button className="button button__main" onClick={this.updateChar}>
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
@@ -76,10 +75,12 @@ class RandomChar extends Component {
 const View = ({char}) => {
         
     const {name, description, thumbnail, homepage, wiki} = char;
+    
+    const notAvailable = thumbnail.includes('image_not_available') ? '_not-available' : '';
 
     return (
         <div className="randomchar__block">
-            <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+            <img src={thumbnail} alt="Random character" className={`randomchar__img${notAvailable}`}/>
             <div className="randomchar__info">
                 <p className="randomchar__name">{name}</p>
                 <p className="randomchar__descr">
